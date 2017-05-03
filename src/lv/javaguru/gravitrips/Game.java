3aa15d44fefe5a54cb61fd5b85d7fe21fd9a95b9
Vasily <vasilykom@gmail.com>
2017-05-04 00:52:38 +0300

    update
    less classes

--- src/lv/javaguru/gravitrips/Game.java
+++ src/lv/javaguru/gravitrips/Game.java
@@ -1,42 +1,136 @@
 package lv.javaguru.gravitrips;
 
+import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Game {
 
     private Player player1;
     private Player player2;
     private Player currentPlayer;
-    private boolean gameOver = false;
     private Board board = new Board();
+     boolean gameOver;
+    private int gameTypeNr;
+    boolean playerChoosedGameType = false;
 
-
-    public Game(Player player1, Player player2) {
-        this.player1 = player1;
-        this.player2 = player2;
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.startGame();
     }
 
-    public void startGame() {
+    void startGame() {
+        showGameSettingsMenu();
         currentPlayer = player1;
         board.resetBoardToNew();
         board.printBoardToConsole();
         board.makeNewListOfValidColumnNumbers();
+        while (!gameOver) {
 
-        while (true) {
             board.setSymbolOfPlayer();
             board.checkWhichRowIsAvailable(currentPlayer.move(board.createListOfValidColumns()));
-            if(board.createListOfValidColumns().isEmpty()){
+            if (board.createListOfValidColumns().isEmpty()) {
                 System.out.println("Nobody won!!! Please replay the Game!!!\n");
-                SetUp setUp = new SetUp();
-                setUp.setUpGameSettings();
-
-            } else if (board.checkAllDiagonalsForWinner()){
-                System.out.println("The winner is = " +currentPlayer.getName()+ " !!!\n");
-                SetUp setUp = new SetUp();
-                setUp.setUpGameSettings();
-
+                showGameSettingsMenu();
+                startGame();
+            } else if (board.checkAllDiagonalsForWinner()) {
+                System.out.println("The winner is = " + currentPlayer.getName() + " !!!\n");
+                showGameSettingsMenu();
+                startGame();
             }
             switchPlayer();
-
         }
+    }
+
+    void showGameSettingsMenu() {
+        showGameGreeting();
+        showGameModes();
+        getGamePlayOptionNumber();
+        while (!playerChoosedGameType) {
+            if (gameTypeNr == 1) {
+                proceedGameOption1();
+            } else if (gameTypeNr == 2) {
+                proceedGameOption2();
+            } else if (gameTypeNr == 3) {
+                proceedGameOption3();
+            } else if (gameTypeNr == 4) {
+                proceedGameOption4();
+            }
+        }
+    }
+
+    void proceedGameOption1() {
+        player1 = new HumanPlayer("X");
+        player2 = new AiPlayer("O");
+        System.out.println("Game is started!");
+        System.out.println("\n**** Player vs Computer ****");
+        playerChoosedGameType = true;
+    }
+
+    void proceedGameOption2() {
+        System.out.println("***Game is started!***");
+        System.out.println("\n**** Computer vs Player ****");
+        player1 = new AiPlayer("X");
+        player2 = new HumanPlayer("O");
+        playerChoosedGameType = true;
+    }
+
+    void proceedGameOption3() {
+        System.out.println("***Game is started!***");
+        System.out.println("\n**** Player vs Player ****");
+        player1 = new HumanPlayer("X");
+        player2 = new HumanPlayer("O");
+
+        playerChoosedGameType = true;
+    }
+
+    void proceedGameOption4() {
+        System.out.println("***Game is started!***");
+        System.out.println("\n**** Computer vs Computer ****");
+        player1 = new AiPlayer("X");
+        player2 = new AiPlayer("O");
+        playerChoosedGameType = true;
+    }
+
+    void getGamePlayOptionNumber() {
+        Scanner scanner = new Scanner(System.in);
+        int inputNumber;
+        while (true) {
+            try {
+                System.out.println();
+                System.out.print("Input Game Play option number: ");
+                inputNumber = Integer.parseInt(scanner.nextLine());
+                Set<Integer> set;
+                set = IntStream
+                        .range(1, 5)
+                        .boxed()
+                        .collect(Collectors.toSet());
+
+                if (set.contains(inputNumber)) {
+                    break;
+                }
+            } catch (NumberFormatException e) {
+            }
+        }
+        gameTypeNr = inputNumber;
+    }
+
+    void showGameModes() {
+
+        System.out.println("1-Player vs Computer");
+        System.out.println("2-Computer vs Player");
+        System.out.println("3-Player vs Player");
+        System.out.println("4-Computer vs Computer");
+    }
+
+    void showGameGreeting() {
+        System.out.println("************************");
+        System.out.println("*Welcome to GRAVITRIPS!*");
+        System.out.println("************************");
+        System.out.println("");
+        System.out.println("Select Game mode:");
+        System.out.println("");
     }
 
     private void switchPlayer() {
@@ -46,4 +140,6 @@ public class Game {
             currentPlayer = player1;
         }
     }
+
+
 }
--- src/lv/javaguru/gravitrips/Start.java
+++
@@ -1,9 +0,0 @@
-package lv.javaguru.gravitrips;
-
-
-public class Start {
-    public static void main(String[] args) {
-        SetUp setUp = new SetUp();
-        setUp.setUpGameSettings();
-    }
-}
